137173b1360f97f2d9305302e76e999f38a93028
ilyabobrusev@e16 <ilyabobrusev@dom.com>
2025-03-10 22:39:38 +0300

    Revert "Add progress v2.4"
    This reverts commit 7edbb17f8a69b4975c094e5ae6e5459d5baad530.

--- v2/4/4/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/user")
-async def login(username: str, age: int) -> dict:
-    return {"user": username, "age": age}
--- v2/4/tasks/1/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/product/{product_id}")
-async def detail_view(product_id: int) -> dict:
-    return {"product": f'Stock number {product_id}'}
